0e30b0d168a143da313e43ac0540e5bc76bb4d1f
ginsonsitocoolboy <koichiro.moeller@tbz.edu.ch>
2026-06-05 16:14:00 +0200

    6

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,16 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.0"
     }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.0"
+    }
   }
 
   required_version = ">= 1.5.0"
@@ -13,11 +23,37 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "tls_private_key" "m300_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_sensitive_file" "private_key" {
+  filename        = "${path.module}/m300-key.pem"
+  content         = tls_private_key.m300_key.private_key_pem
+  file_permission = "0600"
+}
+
+resource "local_file" "public_key" {
+  filename = "${path.module}/m300-key.pub"
+  content  = tls_private_key.m300_key.public_key_openssh
+}
+
+resource "aws_key_pair" "m300_key" {
+  key_name   = "m300-key"
+  public_key = tls_private_key.m300_key.public_key_openssh
+
+  tags = {
+    Name    = "m300-key"
+    Project = "Modul300"
+  }
+}
+
 resource "aws_instance" "m300_webapp" {
   ami           = "ami-05cf1e9f73fbad2e2"
   instance_type = "t2.micro"
 
-  key_name = "m300-key"
+  key_name = aws_key_pair.m300_key.key_name
 
   vpc_security_group_ids = ["sg-0f5f0eabbcc268057"]
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,17 +8,27 @@ output "instance_public_dns" {
   value       = aws_instance.m300_webapp.public_dns
 }
 
-output "ssh_command" {
-  description = "SSH Befehl fuer die Verbindung"
-  value       = "ssh -i C:/Users/koichiro.moeller/.ssh/m300-key ubuntu@${aws_instance.m300_webapp.public_ip}"
+output "ssh_command_public_ip" {
+  description = "SSH Befehl mit Public IP"
+  value       = "ssh -i ${abspath("${path.module}/m300-key.pem")} ubuntu@${aws_instance.m300_webapp.public_ip}"
 }
 
-output "website_url" {
-  description = "URL der Web-App"
+output "ssh_command_elastic_ip" {
+  description = "SSH Befehl mit Elastic IP"
+  value       = "ssh -i ${abspath("${path.module}/m300-key.pem")} ubuntu@98.94.188.179"
+}
+
+output "website_url_public_ip" {
+  description = "URL der Web-App mit Public IP"
   value       = "http://${aws_instance.m300_webapp.public_ip}"
 }
 
-output "monitoring_url" {
-  description = "URL von Uptime Kuma"
-  value       = "http://${aws_instance.m300_webapp.public_ip}:3001"
+output "website_url_elastic_ip" {
+  description = "URL der Web-App mit Elastic IP"
+  value       = "http://98.94.188.179"
+}
+
+output "monitoring_url_elastic_ip" {
+  description = "URL von Uptime Kuma mit Elastic IP"
+  value       = "http://98.94.188.179:3001"
 }
